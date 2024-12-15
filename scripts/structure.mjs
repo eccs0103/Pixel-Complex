5e1808bf9e5847ae7dcaa83928f2e0076ad0be60
Arman <eccs0103@gmail.com>
2024-12-16 01:09:12 +0400

    0.4.0

--- scripts/structure.mjs
+++ scripts/structure.mjs
@@ -1,9 +1,18 @@
-// @ts-ignore
-/** @typedef {import("./modules/archive.js")} */
-// @ts-ignore
-/** @typedef {import("./modules/application.js")} */
-
 "use strict";
+
+import { } from "./core/extensions.mjs";
+import { } from "./core/generators.mjs";
+import { } from "./core/measures.mjs";
+import { } from "./core/palette.mjs";
+
+import { } from "./workers/extensions.mjs";
+import { } from "./workers/generators.mjs";
+import { } from "./workers/measures.mjs";
+
+import { } from "./dom/extensions.mjs";
+import { } from "./dom/generators.mjs";
+import { } from "./dom/palette.mjs";
+import { } from "./dom/storage.mjs";
 
 /**
  * @callback ModifierCallback
@@ -170,3 +179,5 @@ class Filter {
 	}),
 	//#endregion
 };
+
+export { };
